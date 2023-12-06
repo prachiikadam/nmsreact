dd3a86d1b1bd4b6873e86e6d836ac339873de584
Prachi Kadam <pb79kadam@gmail.com>
2023-12-06 08:53:56 +0530

    Laying the foundation

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const heading = React.createElement('h1',{},'Hello world')
 console.log(heading)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-//render method takes react object  and ocnvert it into h1 tag and put it up in dom
+//render method takes react elemnt whict is nothing but js object  and ocnvert it into h1 tag and put it up in dom
 // root.render(heading)
 
 /**
@@ -21,8 +21,38 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
  */
 
 
-const heading2 = React.createElement('div',{id : "parent"},React.createElement('div',{id :'child'},[React.createElement('h1',{},'gsg'),React.createElement('h1',{},'Namaste react launch!!!!!')]))
-root.render(heading2)
+// const heading2 = React.createElement('div',{id : "parent"},React.createElement('div',{id :'child'},[React.createElement('h1',{},'gsg'),React.createElement('h1',{},'Namaste react launch!!!!!')]))
+// console.log('react element',heading2)
+// //JSX
+// const jsxheading = <h1 id="heading"> jsx heading react</h1>
+// console.log('jsx',jsxheading)
+// root.render(heading2)
+
+
+/**
+ * React element and react function
+ */
+
+
+const Title = (
+    <h1>React elemnet</h1>
+)
+
+// const Title = () =>(
+//     <h1>React elemnet</h1>
+// )
+
+//component composition
+
+const HeadingComponent = () =>{
+    return <div id="container">
+        {/* <Title></Title> */}
+        {Title}
+        <h1>React functional component</h1>
+    </div>
+}
+
+root.render(<HeadingComponent/>)
 
 
 
